849e539ba82180cb7e3bffd400171345ed93aa51
aryansingh-123 <aksingh2558@gmail.com>
2024-08-09 16:36:33 +0530

    First code

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+$name="Aryan Singh"; //Defining like variable and stored
+$color = "green"; 
+echo $name;// printed echo is use to print 
+?>
+<h2 style="color:red"><?php echo $name?></h2>
+<h2 style="color:<?php echo $color?>"><?php echo $name?></h2>
